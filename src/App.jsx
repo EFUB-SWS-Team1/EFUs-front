e593272d75f6b25994a7f957a02d4909a4698f25
sunny6312 <sooyeon6312@naver.com>
2026-08-02 06:37:56 +0900

    가계부 거래 상세(고쳐야함), 거래 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import LedgerCreatePage from "./pages/ledger/LedgerCreatePage";
 import ExpensePage from "./pages/ledger/ExpensePage";
+import ExpenseDetailPage from "./pages/ledger/ExpenseDetailPage";
+import ExpenseEditPage from "./pages/ledger/ExpenseEditPage";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
           <Route index element={<LedgerCreatePage />} />
           <Route path="ledger" element={<LedgerCreatePage />} />
           <Route path="expense" element={<ExpensePage />} />
+          <Route path="/expense-detail" element={<ExpenseDetailPage />} />
+          <Route path="/expense-edit" element={<ExpenseEditPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
