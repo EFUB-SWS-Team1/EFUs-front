b58b0a5bd18a66aff32e2e99ed712e6b128591d8
seunghyema <seunghyema53@gmail.com>
2026-08-04 19:55:08 +0900

    mock 데이터 true 로 바꾼 버전

--- src/api/event.js
+++ src/api/event.js
@@ -8,7 +8,7 @@ import axiosInstance from "./axiosInstance";
  * USE_MOCK = false → 실제 API
  */
 
-const USE_MOCK = false;
+const USE_MOCK = true;
 
 const MOCK_EVENTS_BY_GENERATION = {
   "efub-6": {
--- src/api/groupManage.js
+++ src/api/groupManage.js
@@ -7,7 +7,7 @@ import axiosInstance from "./axiosInstance";
  * USE_MOCK = false → 실제 API
  */
 
-const USE_MOCK = false;
+const USE_MOCK = true;
 
 const MOCK_DATA = {
   "efub-6": {
