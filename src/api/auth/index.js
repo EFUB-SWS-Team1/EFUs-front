deb815c1abf7ce1c916a678442b8c1c92c3bb233
eluda315 <michelle3hg@gmail.com>
2026-08-07 01:29:28 +0900

    [FEAT] auth 로그아웃 API 실연동

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,3 +1,4 @@
 export { getKakaoAuthorizeUrl, loginWithKakaoCode } from "./kakaoLogin";
 export { reissue } from "./reissue";
 export { getCurrentUser } from "./user";
+export { kakaoLogout } from "./kakaoLogout";
